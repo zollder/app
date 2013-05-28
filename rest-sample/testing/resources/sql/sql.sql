aefb3e140b5ab14ec2b7709f4194b3e77a73e94d
Eugene Simon <eugen@publicbikesystem.com>
2013-05-28 10:28:57 -0400

    BUILD: - added database-reset(redeploy-schema) support to the ant deploy target; - modified properties accordingly;

--- rest-sample/testing/resources/sql/sql.sql
+++ rest-sample/testing/resources/sql/sql.sql
@@ -1,3 +1,7 @@
+-- Dumping database structure for test
+DROP DATABASE IF EXISTS `test`;
+CREATE DATABASE IF NOT EXISTS `test`;
+USE `test`;
 
 DROP TABLE IF EXISTS `contacts`;
 CREATE TABLE  `contacts`
@@ -13,21 +17,16 @@ CREATE TABLE  `contacts`
   PRIMARY KEY (id)
 );
 
-INSERT INTO `contacts`
-VALUES
-    (1,'name 1','address 1','F','2000-04-16','test1@email.com','5145145111','5145145121'),
-    (2,'name 2','address 2','F','2000-04-17','test2@email.com','5145145112','5145145122'),
-    (3,'name 3','address 3','M','2000-04-18','test3@email.com','5145145113','5145145123');
-
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE  `user`
 (
-  primaryKey int(10) unsigned NOT NULL AUTO_INCREMENT,
+  primaryKey int(10) unsigned NOT NULL UNIQUE AUTO_INCREMENT,
   firstName varchar(45) NOT NULL,
   lastName varchar(45) NOT NULL,
-  userName varchar(45) NOT NULL,
+  userName varchar(45) NOT NULL UNIQUE,
   password varchar(80),
   email varchar(45) DEFAULT NULL,
+  isEnabled boolean NOT NULL,
   canLogin boolean NOT NULL,
   isAdmin boolean NOT NULL,
   PRIMARY KEY (primaryKey)
