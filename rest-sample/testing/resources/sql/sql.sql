f1b1451fa05d448f2870e314bbe5d42000600db3
Oleg <oleg.adamenko@gmail.com>
2013-07-01 01:42:19 -0500

    added the table for devices added the ywire xml tentative map for properties table for device holds 2 ip fields that will be decided which will stay.

--- rest-sample/testing/resources/sql/sql.sql
+++ rest-sample/testing/resources/sql/sql.sql
@@ -32,6 +32,24 @@ CREATE TABLE  `user`
   PRIMARY KEY (primaryKey)
 );
 
+DROP TABLE IF EXISTS `devices`;
+CREATE TABLE `devices`
+(
+  id INT(10) UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT ,
+  type ENUM('Y1G','Y1G-M','YFM','YFM2','YFM4') NOT NULL ,
+  ip INT(10) UNSIGNED NOT NULL UNIQUE,
+  ip2 varchar(15) NOT NULL UNIQUE,
+  PRIMARY KEY (id)
+);
+
 INSERT INTO `user`
 VALUES
     (1,'Admin','User','adminuser','d9acd48369a1a20280c8c3b6921d8d8a','admin@email.com',1,1,1);
+INSERT INTO test.devices (type,ip,ip2)
+VALUES ('Y1G',125425,'192.168.168.1');
+INSERT INTO test.devices (type,ip,ip2)
+VALUES ('Y1G-M',125426,'192.168.168.2');
+INSERT INTO test.devices (type,ip,ip2)
+VALUES ('YFM',125427,'192.168.168.3');
+INSERT INTO test.devices (type,ip,ip2)
+VALUES ('YFM2',125428,'192.168.168.4');
